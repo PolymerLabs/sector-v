1061a1ce2c46bf709ffee8cc90aaf7173c5f42a6
Elliott Marquez <emarquez@google.com>
2019-11-01 00:15:53 -0700

    fix initial load state

--- src/components/knd-calculator.ts
+++ src/components/knd-calculator.ts
@@ -1,32 +1,43 @@
 import '@material/mwc-button/mwc-button';
 import '@material/mwc-icon/mwc-icon';
 
-import {Button} from '@material/mwc-button/mwc-button';
-import {css, customElement, html, property} from 'lit-element';
+import { Button } from '@material/mwc-button/mwc-button';
+import {
+  css,
+  customElement,
+  html,
+  property,
+  query,
+  PropertyValues
+} from 'lit-element';
 
-import {fontSize2x, fontSize3x} from '../util/base-styles';
+import { fontSize2x, fontSize3x } from '../util/base-styles';
 
-import {KndWidgetBase} from './knd-widget-base';
+import { KndWidgetBase } from './knd-widget-base';
+import { WidgetSize } from '../util/types';
 
-type Operator = '+'|'-'|'*'|'/';
+type Operator = '+' | '-' | '*' | '/';
 
 @customElement('knd-widget-calculator')
 export class Calculator extends KndWidgetBase {
   static get styles() {
-    const styles = [css`
-      #title {
-        font-size: ${fontSize3x};
-        padding-left: 23px;
-      }
-      mwc-button, #display {
-        font-size: ${fontSize2x} !important;
-      }
-      #display {
-        display: flex;
-        justify-content: space-between;
-        padding: 23px;
-      }
-    `];
+    const styles = [
+      css`
+        #title {
+          font-size: ${fontSize3x};
+          padding-left: 23px;
+        }
+        mwc-button,
+        #display {
+          font-size: ${fontSize2x} !important;
+        }
+        #display {
+          display: flex;
+          justify-content: space-between;
+          padding: 23px;
+        }
+      `
+    ];
 
     let superStyles = super.styles;
     if (!Array.isArray(superStyles)) {
@@ -35,13 +46,17 @@ export class Calculator extends KndWidgetBase {
     return superStyles.concat(styles);
   }
 
-  @property({type: Number}) numberInput = 0;
+  @property({ type: Number }) numberInput = 0;
   private resultSoFar = 0;
   private begunTypingNewNumber = true;
-  @property({}) displayedOperator: Operator|'' = '';
+  @property({}) displayedOperator: Operator | '' = '';
   private currentOperator: Operator = '+';
   private previousOperator: Operator = '+';
   private previousNumber = 0;
+  private originalX = 0;
+
+  @query('#buttons') buttonsFullWidth!: HTMLDivElement | null;
+  @query('#buttons span') buttonsWrapper!: HTMLSpanElement | null;
 
   clear() {
     this.numberInput = 0;
@@ -114,7 +129,7 @@ export class Calculator extends KndWidgetBase {
     // If there's no current operator, we're adding a digit onto the current
     // number.
     if (this.begunTypingNewNumber) {
-      this.numberInput = (this.numberInput * 10) + digit;
+      this.numberInput = this.numberInput * 10 + digit;
     } else {
       this.begunTypingNewNumber = true;
       this.resultSoFar = this.numberInput;
@@ -144,7 +159,7 @@ export class Calculator extends KndWidgetBase {
     }
   }
 
-  handleOperator(operator: Operator|'=') {
+  handleOperator(operator: Operator | '=') {
     if (operator !== '=') {
       this.previousOperator = operator;
       this.previousNumber = this.numberInput;
@@ -153,14 +168,20 @@ export class Calculator extends KndWidgetBase {
     }
 
     this.resultSoFar = this.applyOperation(
-        this.resultSoFar, this.numberInput, this.currentOperator);
+      this.resultSoFar,
+      this.numberInput,
+      this.currentOperator
+    );
     this.numberInput = this.resultSoFar;
     if (operator === '=') {
       if (!this.begunTypingNewNumber) {
         // all of the above was a no op, but we want to apply the previous
         // operation again
         this.numberInput = this.applyOperation(
-            this.resultSoFar, this.previousNumber, this.previousOperator);
+          this.resultSoFar,
+          this.previousNumber,
+          this.previousOperator
+        );
       }
       this.displayedOperator = '';
       this.currentOperator = '+';
@@ -179,6 +200,52 @@ export class Calculator extends KndWidgetBase {
     }
   }
 
+  protected resizeListener = () => {
+    const full = this.buttonsFullWidth;
+    const wrapper = this.buttonsWrapper;
+    if (!full || !wrapper) {
+      return;
+    }
+
+    const fullRect = full.getBoundingClientRect();
+    const wrapperRect = wrapper.getBoundingClientRect();
+
+    const fullX = ((fullRect as unknown) as ClientRect).width;
+    let wrapperX = this.originalX;
+    if (!this.originalX) {
+      wrapperX = ((wrapperRect as unknown) as ClientRect).width;
+      this.originalX = wrapperX;
+    }
+
+    const scale = fullX / wrapperX;
+    wrapper.style.zoom = '' + scale;
+  };
+
+  async updated(changedProps: PropertyValues) {
+    const sizeChanged = changedProps.has('size');
+
+    if (sizeChanged) {
+      const oldSize = changedProps.get('size') as WidgetSize | undefined;
+      const newSize = this.size;
+
+      if (oldSize === 'large') {
+        window.removeEventListener('reset', this.resizeListener);
+        const wrapper = this.buttonsWrapper;
+        if (wrapper) {
+          wrapper.style.zoom = '1';
+        }
+      }
+
+      if (newSize === 'large') {
+        await this.firstUpdated;
+        this.resizeListener();
+        window.addEventListener('resize', this.resizeListener, {
+          passive: true
+        });
+      }
+    }
+  }
+
   connectedCallback() {
     super.connectedCallback();
     this.addEventListener('keydown', this.keydownHandler);
@@ -186,9 +253,11 @@ export class Calculator extends KndWidgetBase {
   }
 
   disconnectedCallback() {
-    this.disconnectedCallback();
+    super.disconnectedCallback();
     this.removeEventListener('keydown', this.keydownHandler);
     this.removeEventListener('click', this.clickHandler);
+
+    window.removeEventListener('reset', this.resizeListener);
   }
 
   renderTiny() {
@@ -212,29 +281,33 @@ export class Calculator extends KndWidgetBase {
 
         <div>${this.numberInput.toLocaleString()}</div>
       </div>
-      <mwc-button>7</mwc-button>
-      <mwc-button>8</mwc-button>
-      <mwc-button>9</mwc-button>
-      <mwc-button>/</mwc-button>
-      <br>
-      <mwc-button>4</mwc-button>
-      <mwc-button>5</mwc-button>
-      <mwc-button>6</mwc-button>
-      <mwc-button>*</mwc-button>
-      <br>
-      <mwc-button>1</mwc-button>
-      <mwc-button>2</mwc-button>
-      <mwc-button>3</mwc-button>
-      <mwc-button>-</mwc-button>
-      <br>
-      <mwc-button>0</mwc-button>
-      <mwc-button disabled> </mwc-button>
-      <mwc-button>=</mwc-button>
-      <mwc-button>+</mwc-button>
+      <div id="buttons">
+        <span>
+          <mwc-button>7</mwc-button>
+          <mwc-button>8</mwc-button>
+          <mwc-button>9</mwc-button>
+          <mwc-button>/</mwc-button>
+          <br />
+          <mwc-button>4</mwc-button>
+          <mwc-button>5</mwc-button>
+          <mwc-button>6</mwc-button>
+          <mwc-button>*</mwc-button>
+          <br />
+          <mwc-button>1</mwc-button>
+          <mwc-button>2</mwc-button>
+          <mwc-button>3</mwc-button>
+          <mwc-button>-</mwc-button>
+          <br />
+          <mwc-button>0</mwc-button>
+          <mwc-button disabled> </mwc-button>
+          <mwc-button>=</mwc-button>
+          <mwc-button id="last">+</mwc-button>
+        </span>
+      </div>
     `;
   }
 
   renderLarge() {
-    return html``;
+    return this.renderMedium();
   }
 }
