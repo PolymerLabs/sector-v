9907fd5abfc28f4e1001875eefc85d3ccd6902d2
Elliott Marquez <emarquez@google.com>
2019-10-31 22:31:22 -0700

    only one place to create new widget

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,22 +1,24 @@
-import { html } from "lit-element";
-import { WidgetRenderers } from "./types";
+import { html } from 'lit-element';
+import { WidgetCatalog } from './types';
 
-
-
-export const WIDGET_RENDERERS: WidgetRenderers = {
-  'test-widget': (widgetId, size) => html`
-      <test-widget
-          size=${size}
-          widgetId=${widgetId}>
-      </test-widget>`,
-  'knd-widget-hue': (widgetId, size) => html`
-      <knd-widget-hue
-          size=${size}
-          widgetId=${widgetId}>
-      </knd-widget-hue>`,
-  'knd-widget-calculator': (widgetId, size) => html`
-      <knd-widget-calculator
-          size=${size}
-          widgetId=${widgetId}>
-      </knd-widget-calculator>`,
-}
+export const WIDGETS: WidgetCatalog = {
+  'test-widget': {
+    friendlyName: 'To-Do',
+    renderer: (widgetId, size) => html`
+      <test-widget size=${size} widgetId=${widgetId}> </test-widget>
+    `
+  },
+  'knd-widget-hue': {
+    friendlyName: 'Huey',
+    renderer: (widgetId, size) => html`
+      <knd-widget-hue size=${size} widgetId=${widgetId}> </knd-widget-hue>
+    `
+  },
+  'knd-widget-calculator': {
+    friendlyName: 'Calculon',
+    renderer: (widgetId, size) => html`
+      <knd-widget-calculator size=${size} widgetId=${widgetId}>
+      </knd-widget-calculator>
+    `
+  }
+};
